f77565d4c609598797fea692a49b3e4baae3d6b1
Andrejs <andrejs.cunskis@gmail.com>
2021-05-02 23:15:21 +0300

    Do not fail on pr description update (#44)

--- lib/allure_report_publisher/lib/uploaders/_uploader.rb
+++ lib/allure_report_publisher/lib/uploaders/_uploader.rb
@@ -88,7 +88,7 @@ module Publisher
         return unless ci_provider
 
         log("Adding allure report link to pr description")
-        Helpers::Spinner.spin("adding") do
+        Helpers::Spinner.spin("adding", exit_on_error: false) do
           ci_provider.add_report_url
         end
       end
