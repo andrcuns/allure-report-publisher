a6d90ac4c1e3c25f3c3881fdc6646137d083ac8a
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-05-06 22:31:44 +0300

    Fix url for public gcs report

--- lib/allure_report_publisher/lib/uploaders/gcs.rb
+++ lib/allure_report_publisher/lib/uploaders/gcs.rb
@@ -97,7 +97,7 @@ module Publisher
       # @param [String] path_prefix
       # @return [String]
       def url(path_prefix)
-        ["https://storage.cloud.google.com", bucket_name, path_prefix, "index.html"].compact.join("/")
+        ["https://storage.googleapis.com", bucket_name, path_prefix, "index.html"].compact.join("/")
       end
     end
   end
