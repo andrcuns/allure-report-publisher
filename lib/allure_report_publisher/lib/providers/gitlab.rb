098f1d56b24772776cc17290cc176f26af7b83da
Andrejs <andrejs.cunskis@gmail.com>
2021-05-21 09:38:38 +0300

    Use short sha in report message, fix gitlab diff url (#98)
    * Fix gitlab mr sha diff link
    
    * Use short sha in description

--- lib/allure_report_publisher/lib/providers/gitlab.rb
+++ lib/allure_report_publisher/lib/providers/gitlab.rb
@@ -122,8 +122,8 @@ module Publisher
       #
       # @return [String]
       def sha_url
-        sha = ENV["CI_COMMIT_SHA"]
-        short_sha = ENV["CI_COMMIT_SHORT_SHA"]
+        sha = ENV["CI_MERGE_REQUEST_SOURCE_BRANCH_SHA"] || ENV["CI_COMMIT_SHA"]
+        short_sha = sha[0..7]
 
         "[#{short_sha}](#{server_url}/#{project}/-/merge_requests/#{mr_iid}/diffs?commit_id=#{sha})"
       end
--- spec/allure_report_publisher/lib/providers/gitlab_spec.rb
+++ spec/allure_report_publisher/lib/providers/gitlab_spec.rb
@@ -5,14 +5,13 @@ RSpec.describe Publisher::Providers::Gitlab do
   let(:auth_token) { "token" }
   let(:event_name) { "merge_request_event" }
   let(:update_pr) { "description" }
-  let(:sha) { "e1de5e18c3af8skjdhfksjdhjk" }
-  let(:short_sha) { "e1de5e18c3af8" }
   let(:mr_id) { "1" }
   let(:project) { "andrcuns/allure-report-publisher" }
   let(:comment_double) { double("comments", auto_paginate: [comment].compact) }
   let(:comment) { nil }
+  let(:sha) { "cfdef23b4b06df32ab1e98ee4091504948daf2a9" }
   let(:sha_url) do
-    "[#{short_sha}](#{env[:CI_SERVER_URL]}/#{project}/-/merge_requests/#{mr_id}/diffs?commit_id=#{sha})"
+    "[#{sha[0..7]}](#{env[:CI_SERVER_URL]}/#{project}/-/merge_requests/#{mr_id}/diffs?commit_id=#{sha})"
   end
   let(:urls) do
     <<~URLS
@@ -37,8 +36,7 @@ RSpec.describe Publisher::Providers::Gitlab do
       CI_MERGE_REQUEST_IID: mr_id,
       CI_PIPELINE_SOURCE: event_name,
       GITLAB_AUTH_TOKEN: auth_token,
-      CI_COMMIT_SHA: sha,
-      CI_COMMIT_SHORT_SHA: short_sha
+      CI_MERGE_REQUEST_SOURCE_BRANCH_SHA: sha
     }.compact
   end
 
