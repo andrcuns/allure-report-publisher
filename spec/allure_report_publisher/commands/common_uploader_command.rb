8569d409dd52e2efa41b97b7f8e88b457cbf8756
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2022-04-15 12:20:48 +0300

    Add spec for --summary option

--- spec/allure_report_publisher/commands/common_uploader_command.rb
+++ spec/allure_report_publisher/commands/common_uploader_command.rb
@@ -39,15 +39,13 @@ RSpec.shared_examples("upload command") do
     allow(Publisher::Helpers).to receive(:validate_allure_cli_present)
   end
 
-  context "with required args" do
+  context "with required args", :aggregate_failures do
     it "executes uploader" do
       run_cli(*command, *cli_args)
 
-      aggregate_failures do
-        expect(uploader).to have_received(:new).with(args)
-        expect(uploader_stub).to have_received(:generate_report)
-        expect(uploader_stub).to have_received(:upload)
-      end
+      expect(uploader).to have_received(:new).with(args)
+      expect(uploader_stub).to have_received(:generate_report)
+      expect(uploader_stub).to have_received(:upload)
     end
 
     it "executes uploader without prefix argument" do
@@ -63,16 +61,14 @@ RSpec.shared_examples("upload command") do
     end
   end
 
-  context "with optional args" do
+  context "with optional args", :aggregate_failures do
     it "executes uploader with --update-pr=comment" do
       run_cli(*command, *cli_args, "--update-pr=comment")
 
-      aggregate_failures do
-        expect(uploader).to have_received(:new).with({ **args, update_pr: "comment" })
-        expect(uploader_stub).to have_received(:generate_report)
-        expect(uploader_stub).to have_received(:upload)
-        expect(uploader_stub).to have_received(:add_url_to_pr)
-      end
+      expect(uploader).to have_received(:new).with({ **args, update_pr: "comment" })
+      expect(uploader_stub).to have_received(:generate_report)
+      expect(uploader_stub).to have_received(:upload)
+      expect(uploader_stub).to have_received(:add_url_to_pr)
     end
 
     it "executes uploader with --update-pr=description" do
@@ -95,9 +91,20 @@ RSpec.shared_examples("upload command") do
         expect(uploader_stub).to have_received(:upload)
       end
     end
+
+    it "executes uploader with --summary=behaviors" do
+      run_cli(*command, *cli_args, "--update-pr=comment", "--summary=behaviors")
+
+      aggregate_failures do
+        expect(uploader).to have_received(:new).with({ **args, update_pr: "comment", summary_type: "behaviors" })
+        expect(uploader_stub).to have_received(:generate_report)
+        expect(uploader_stub).to have_received(:upload)
+        expect(uploader_stub).to have_received(:add_url_to_pr)
+      end
+    end
   end
 
-  context "with missing args" do
+  context "with missing args", :aggregate_failures do
     it "exits when result glob is missing" do
       expect { run_cli(*command, cli_args[1]) }.to raise_error(SystemExit)
       expect(uploader_stub).not_to have_received(:generate_report)
