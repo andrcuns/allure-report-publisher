74bca71b526ee7c8502c965e3f34a399ba41f7dc
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2022-11-07 11:58:50 +0200

    Revert "gsutil performance improvements (#392)"
    This reverts commit 83045222f633810a68b530b2687e59be4cbf737a.

--- lib/allure_report_publisher/lib/helpers/gsutil.rb
+++ lib/allure_report_publisher/lib/helpers/gsutil.rb
@@ -63,9 +63,7 @@ module Publisher
           execute_shell([
             base_cmd(key_file),
             "-h 'Cache-Control:private, max-age=#{cache_control}'",
-            "rsync",
-            "-j json,csv,txt,js,css",
-            "-r #{source_dir} gs://#{bucket}/#{destination_dir}"
+            "rsync -r #{source_dir} gs://#{bucket}/#{destination_dir}"
           ].join(" "))
         end
         log_debug("Finished upload successfully")
