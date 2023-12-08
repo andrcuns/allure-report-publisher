61cfd9d36a890734cfca6c58bebb2b437f219e35
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2023-12-08 11:29:11 +0200

    Fix complexity violation

--- lib/allure_report_publisher/lib/providers/gitlab.rb
+++ lib/allure_report_publisher/lib/providers/gitlab.rb
@@ -16,76 +16,25 @@ module Publisher
                      :server_url,
                      :build_name
 
+      # Get gitlab client
+      #
+      # @return [Gitlab::Client]
+      def client
+        @client ||= begin
+          raise("Missing GITLAB_AUTH_TOKEN environment variable!") unless env("GITLAB_AUTH_TOKEN")
+
+          ::Gitlab::Client.new(
+            endpoint: "#{server_url}/api/v4",
+            private_token: env("GITLAB_AUTH_TOKEN")
+          )
+        end
+      end
+
       # Current pull request description
       #
       # @return [String]
       def pr_description
         @pr_description ||= client.merge_request(project, mr_iid).description
-      end
-
-      # Update pull request description
-      #
-      # @return [void]
-      def update_pr_description
-        log_debug("Updating mr description for mr !#{mr_iid}")
-        client.update_merge_request(
-          project,
-          mr_iid,
-          description: url_section_builder.updated_pr_description(pr_description)
-        )
-      end
-
-      # rubocop:disable Metrics/PerceivedComplexity
-      # Add comment with report url
-      #
-      # @return [void]
-      def add_comment
-        if main_comment
-          log_debug("Updating summary in comment with id #{discussion.id} in mr !#{mr_iid}")
-
-          client.edit_merge_request_note(
-            project,
-            mr_iid,
-            main_comment.id,
-            url_section_builder.comment_body(main_comment.body)
-          )
-        else
-          log_debug("Creating comment with summary for mr ! #{mr_iid}")
-          client.create_merge_request_comment(project, mr_iid, url_section_builder.comment_body)
-        end
-
-        @discussion = nil
-
-        if unresolved_discussion_on_failure && report_has_failures? && !alert_comment
-          client.create_merge_request_discussion_note(project, mr_iid, discussion.id, body: alert_comment_text)
-        elsif alert_comment && !report_has_failures?
-          client.delete_merge_request_discussion_note(project, mr_iid, discussion.id, alert_comment.id)
-        end
-      end
-
-      # Check if allure report has failures
-      #
-      # @return [Boolean]
-      def report_has_failures?
-        main_comment&.body&.include?("❌")
-      end
-
-      # rubocop:enable Metrics/PerceivedComplexity
-
-      # Existing discussion that has comment with allure urls
-      #
-      # @return [Gitlab::ObjectifiedHash]
-      def discussion
-        @discussion ||= client.merge_request_discussions(project, mr_iid).auto_paginate.detect do |discussion|
-          discussion.notes.any? { |note| Helpers::UrlSectionBuilder.match?(note.body) }
-        end
-      end
-
-      # Comment/note with allure urls
-      #
-      # @return [Gitlab::ObjectifiedHash]
-      def main_comment
-        discussion&.notes&.detect { |note| Helpers::UrlSectionBuilder.match?(note.body) }
       end
 
       # Comment with alert text
@@ -103,20 +52,6 @@ module Publisher
       def alert_comment_text
         @alert_comment_text ||=
           env("ALLURE_FAILURE_ALERT_COMMENT") || "There are some test failures that need attention"
-      end
-
-      # Get gitlab client
-      #
-      # @return [Gitlab::Client]
-      def client
-        @client ||= begin
-          raise("Missing GITLAB_AUTH_TOKEN environment variable!") unless env("GITLAB_AUTH_TOKEN")
-
-          ::Gitlab::Client.new(
-            endpoint: "#{server_url}/api/v4",
-            private_token: env("GITLAB_AUTH_TOKEN")
-          )
-        end
       end
 
       # Custom sha
@@ -148,6 +83,81 @@ module Publisher
         short_sha = sha[0..7]
 
         "[#{short_sha}](#{server_url}/#{project}/-/merge_requests/#{mr_iid}/diffs?commit_id=#{sha})"
+      end
+
+      # Update pull request description
+      #
+      # @return [void]
+      def update_pr_description
+        log_debug("Updating mr description for mr !#{mr_iid}")
+        client.update_merge_request(
+          project,
+          mr_iid,
+          description: url_section_builder.updated_pr_description(pr_description)
+        )
+      end
+
+      # Add comment with report url
+      #
+      # @return [void]
+      def add_comment
+        create_or_update_comment
+        create_or_resolve_discussion
+      end
+
+      # Create or update comment with report url
+      #
+      # @return [void]
+      def create_or_update_comment
+        if main_comment
+          log_debug("Updating summary in comment with id #{discussion.id} in mr !#{mr_iid}")
+
+          return client.edit_merge_request_note(
+            project,
+            mr_iid,
+            main_comment.id,
+            url_section_builder.comment_body(main_comment.body)
+          )
+        end
+
+        log_debug("Creating comment with summary for mr ! #{mr_iid}")
+        client.create_merge_request_comment(project, mr_iid, url_section_builder.comment_body)
+      end
+
+      # Create or resolve comment discussion
+      #
+      # @return [void]
+      def create_or_resolve_discussion
+        @discussion = nil
+
+        if unresolved_discussion_on_failure && report_has_failures? && !alert_comment
+          client.create_merge_request_discussion_note(project, mr_iid, discussion.id, body: alert_comment_text)
+        elsif alert_comment && !report_has_failures?
+          client.delete_merge_request_discussion_note(project, mr_iid, discussion.id, alert_comment.id)
+        end
+      end
+
+      # Check if allure report has failures
+      #
+      # @return [Boolean]
+      def report_has_failures?
+        main_comment&.body&.include?("❌")
+      end
+
+      # Existing discussion that has comment with allure urls
+      #
+      # @return [Gitlab::ObjectifiedHash]
+      def discussion
+        @discussion ||= client.merge_request_discussions(project, mr_iid).auto_paginate.detect do |discussion|
+          discussion.notes.any? { |note| Helpers::UrlSectionBuilder.match?(note.body) }
+        end
+      end
+
+      # Comment/note with allure urls
+      #
+      # @return [Gitlab::ObjectifiedHash]
+      def main_comment
+        discussion&.notes&.detect { |note| Helpers::UrlSectionBuilder.match?(note.body) }
       end
     end
   end
