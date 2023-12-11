0419268274e747d041c5aed7c3495ae55b058c02
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2023-12-11 13:18:01 +0200

    Fix mr pipeline detection for child pipelines

--- spec/allure_report_publisher/lib/providers/info/gitlab_spec.rb
+++ spec/allure_report_publisher/lib/providers/info/gitlab_spec.rb
@@ -1,15 +1,15 @@
 RSpec.describe Publisher::Providers::Info::Gitlab, epic: "providers" do
   subject(:pr) { described_class.instance.pr? }
 
+  let(:mr_iid) { nil }
   let(:allure_project) { nil }
   let(:allure_mr_iid) { nil }
-  let(:pipeline_source) { "push" }
 
   let(:env) do
     {
       ALLURE_PROJECT_PATH: allure_project,
       ALLURE_MERGE_REQUEST_IID: allure_mr_iid,
-      CI_PIPELINE_SOURCE: pipeline_source
+      CI_MERGE_REQUEST_IID: mr_iid
     }
   end
 
@@ -18,12 +18,12 @@ RSpec.describe Publisher::Providers::Info::Gitlab, epic: "providers" do
   end
 
   describe "#pr?" do
-    context "with push event" do
+    context "with non merge request pipeline" do
       it { is_expected.to be(false) }
     end
 
-    context "with merge request event" do
-      let(:pipeline_source) { "merge_request_event" }
+    context "with merge request pipeline" do
+      let(:mr_iid) { "1" }
 
       it { is_expected.to be(true) }
     end
