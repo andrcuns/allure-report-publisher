317adc4857a3bfc4b56618fb5e4d47aadf8c1dc1
Andrejs Cunskis <git@andrcuns.mozmail.com>
2026-01-05 13:12:08 +0200

    Add logging for github api operations

--- src/lib/ci/providers/github.ts
+++ src/lib/ci/providers/github.ts
@@ -1,3 +1,4 @@
+import {logger} from '../../../utils/logger.js'
 import {GithubCiInfo} from '../info/github.js'
 import {UrlSectionBuilder} from '../pr/url-section-builder.js'
 import {githubClient} from '../utils.js'
@@ -7,7 +8,7 @@ export class GithubCiProvider extends BaseCiProvider {
   private readonly ciInfo = new GithubCiInfo()
   private readonly client = githubClient
   private _prDescription?: string
-  private _comment?: string | undefined
+  private _comment?: null | string | undefined
 
   public async addReportSection() {}
 
@@ -26,6 +27,7 @@ export class GithubCiProvider extends BaseCiProvider {
   private async getPrDescription() {
     if (this._prDescription) return this._prDescription
 
+    logger.debug(`Fetching PR description for pr '${this.ciInfo.prId}'`)
     const pr = await this.client.rest.pulls.get({
       owner: this.owner,
       repo: this.repo,
@@ -33,19 +35,22 @@ export class GithubCiProvider extends BaseCiProvider {
       pull_number: this.ciInfo.prId!,
     })
     this._prDescription = pr.data.body || ''
+    logger.debug('Fetched PR description')
     return this._prDescription
   }
 
   private async getComment() {
-    if (this._comment) return this._comment
+    if (this._comment !== undefined) return this._comment
 
+    logger.debug(`Fetching PR comment for pr '${this.ciInfo.prId}'`)
     const comments = await this.client.rest.issues.listComments({
       owner: this.owner,
       repo: this.repo,
       // eslint-disable-next-line camelcase
       issue_number: this.ciInfo.prId!,
     })
-    this._comment = comments.data.find((comment) => UrlSectionBuilder.match(comment.body_text))?.body
+    this._comment = comments.data.find((comment) => UrlSectionBuilder.match(comment.body_text))?.body ?? null
+    if (this._comment) logger.debug('Found existing comment with report section')
     return this._comment
   }
 }
