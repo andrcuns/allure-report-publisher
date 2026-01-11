f646b5b4993ca8cd6f8511298f888d1f3d3cf2bf
Andrejs Cunskis <git@andrcuns.mozmail.com>
2026-01-11 14:13:35 +0200

    Add e2e upload test with coverage

--- test/commands/upload/upload.test.ts
+++ test/commands/upload/upload.test.ts
@@ -1,8 +1,13 @@
 import {runCommand} from '@oclif/test'
 
+import {globalConfig} from '../../../src/utils/global-config.js'
 import {expect} from '../../support/setup.js'
 
 describe('upload', () => {
+  beforeEach(() => {
+    globalConfig.disableOutput = false
+  })
+
   describe('help', () => {
     it('prints s3 upload command help', async () => {
       const {stdout} = await runCommand('upload s3 --help')
@@ -17,6 +22,31 @@ describe('upload', () => {
     it('prints gitlab artifacts upload command help', async () => {
       const {stdout} = await runCommand('upload gitlab-artifacts --help')
       expect(stdout).to.contain('Generate report and output GitLab CI artifacts links')
+    })
+  })
+
+  describe('s3', () => {
+    it('runs s3 upload command', async function () {
+      if (process.env.E2E_TEST !== 'true') return this.skip()
+
+      const {AWS_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env
+      if (!AWS_ENDPOINT || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) throw new Error('Missing aws env variables')
+
+      const prefix = `allure-report-publisher/${process.env.GITHUB_REF ?? 'local'}`
+      const args = [
+        '--config=allurerc.mjs',
+        '--bucket=allure-reports',
+        `--prefix=${prefix}`,
+        '--update-pr=comment',
+        '--ci-report-title=Unit Test Report',
+        '--report-name=Unit Test Report',
+        '--add-summary',
+        '--collapse-summary',
+        '--copy-latest',
+        '--debug',
+      ]
+      const {stdout} = await runCommand(`upload s3 ${args.join(' ')}`)
+      expect(stdout).to.match(new RegExp(`${AWS_ENDPOINT}/allure-reports/${prefix}/[\\w/]+/index.html`))
     })
   })
 })
